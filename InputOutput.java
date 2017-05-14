89d5f0e2231ed929cf1d390c810a04776234fd9d
Michal Szukala <szukalagithub@gmail.com>
2017-05-14 23:01:40 +0200

    challenges

--- InputOutput.java
+++ InputOutput.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
  */
 public class InputOutput {
 
-
+// All char in upper case will be in lower case and vice versa
     public void toggleString(){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Give me a string");
@@ -27,10 +27,76 @@ public class InputOutput {
         System.out.println(nowa);
     }
 
+// Will give factors of the number
+    public void factorial() {
+        System.out.println("Give me a number");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        int factorial = number;
+
+        for (int i = 1; i < number; i++)
+            factorial *= (number - i);
+
+        System.out.println(number + "! = " + factorial);
+
+    }
+// After giving string it will check if red backwards will be this same string
+    public void stringBackward(){
+        System.out.println("Give me a string");
+        Scanner scanner = new Scanner(System.in);
+        String word = scanner.next();
+        char[] array = word.toCharArray();
+        int number = array.length - 1;
+        Boolean test = true;
+        for(int i = 0; i < array.length; i++){
+            if(array[i] != array[number])
+                test = false;
+            number--;
+        }
+        if(test)
+            System.out.println("YES");
+        else
+            System.out.println("NO");
+
+    }
+// Check if all the numbers before given number and print them if they are prime numbers
+    public void primeNumber(){
+        System.out.println("Give me a number");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        Boolean test;
+
+        for(int primeNumbers = 2; primeNumbers < number; primeNumbers++) {
+            test = true;
+            for (int i = 2; i < primeNumbers; i++)
+                if (primeNumbers % i == 0)
+                    test = false;
+            if (test == true)
+                System.out.print(primeNumbers + " ");
+        }
+
+
+
+
+    }
+
+    
+
+
+
+
+
 
     public static void main(String[] args){
         InputOutput start = new InputOutput();
-        start.toggleString();
+        //start.primeNumber();
+        for(int i = 1; i != 3; i++)
+            System.out.println(i + " ");
+
+
+
+
+
 
     }
 }
