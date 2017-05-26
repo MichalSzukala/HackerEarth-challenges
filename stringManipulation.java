8f8f2e470c4ebb7a17187cd660f45a1f06345529
Michal Szukala <szukalagithub@gmail.com>
2017-05-26 22:22:17 +0200

    stringManipulation update

--- stringManipulation.java
+++ stringManipulation.java
@@ -17,19 +17,47 @@ public class stringManipulation {
 
     }
 
-    public void splitingInFour() {
+//    public void splitingInFour() {
+//        Scanner scanner = new Scanner(System.in);
+//        int cases = scanner.nextInt();
+//        while (cases > 0) {
+//            //System.out.println("give a string");
+//            String word = scanner.next();
+//            char[] array = word.toCharArray();
+//            int firstIndex = 0;
+//            for(int i = 0; i < word.length(); i++){
+//                if(array[i] == array[i + 1])
+//            }
+//            cases--;
+//        }
+//    }
+//Print string in reverse order
+    public void stringBackword(){
         Scanner scanner = new Scanner(System.in);
         int cases = scanner.nextInt();
         while (cases > 0) {
-            //System.out.println("give a string");
             String word = scanner.next();
-            char[] array = word.toCharArray();
-            int firstIndex = 0;
-            for(int i = 0; i < word.length(); i++){
-                if(array[i] == array[i + 1])
-            }
+            for(int i = word.length() - 1; i >= 0; i--)
+                System.out.print(word.charAt(i));
             cases--;
         }
+    }
+// change the first letters after white space in the sentence
+    public void changeToBigLetter(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("give a string");
+        String word = scanner.nextLine();
+        String newWord = "" + Character.toUpperCase(word.charAt(0));
+        for(int i = 1; i < word.length(); i++){
+            char letter = word.charAt(i);
+            char letterBefore = word.charAt(i-1);
+            Boolean test = Character.isLowerCase(letter);
+            if(test && Character.isWhitespace(letterBefore))
+                newWord +=  Character.toUpperCase(letter);
+            else
+                newWord += letter;
+        }
+        System.out.println(newWord);
     }
 
 }
