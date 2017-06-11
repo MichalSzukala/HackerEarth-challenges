ba486ac0ab59cdb6d941c9be6cb5af8f765ffcef
Michal Szukala <szukalagithub@gmail.com>
2017-06-11 21:47:18 +0200

    searhing update

--- Searching.java
+++ Searching.java
@@ -3,7 +3,26 @@ import java.util.Scanner;
 /**
  * Created by Z on 01/06/2017.
  */
-public class Searching {
+public class Searching{
+//Rest in peace - 21-1!: https://www.hackerearth.com/practice/algorithms/searching/linear-search/practice-problems/algorithm/rest-in-peace-21-1/
+    public void number21(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("give a number");
+        String number = scanner.next();
+        char[] numberArray = number.toCharArray();
+        boolean test = false;
+        for(int i = 0; i < numberArray.length-1; i++)
+            if ((numberArray[i] == '2' && numberArray[i + 1] == '1') || Integer.parseInt(number) % 21 == 0)
+                test = true;
+        if(test)
+            System.out.println("The streak is broken!");
+        else
+            System.out.println("The streak lives still in our heart!");
+    }
+
+
+
+
     //counting vowels in the word
     public void searchVowel(){
         Scanner scanner = new Scanner(System.in);
