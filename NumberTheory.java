cdef640641877bc3d7048095e7258f307de0bfe9
Michal Szukala <szukalagithub@gmail.com>
2017-06-26 20:34:35 +0200

    NumberTheory update

--- NumberTheory.java
+++ NumberTheory.java
@@ -22,4 +22,13 @@ public class NumberTheory {
         }
     }
 
+    public int factorial(int number){
+        if(number == 0)
+            return 1;
+        else
+            return number * factorial(number - 1);
+    }
+
+
+
 }
