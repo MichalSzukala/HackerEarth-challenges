97f6f461f6c1a5b0585c0779bd109ac81e332bfe
Michal Szukala <szukalagithub@gmail.com>
2017-06-27 23:42:30 +0200

    number theory update

--- NumberTheory.java
+++ NumberTheory.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -28,6 +29,35 @@ public class NumberTheory {
         else
             return number * factorial(number - 1);
     }
+
+    public void helpOz() {
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+        int[] array = new int[size];
+        int[] array1 = new int[size];
+        for (int i = 0; i < size; i++)
+            array[i] = scanner.nextInt();
+        array1 = Arrays.copyOf(array, size);
+        Arrays.sort(array1);
+        int max = array1[size - 1];
+        boolean test;
+
+        for (int i = 2; i <= max; i++) {
+            int firstElementMod = array[0] % i;
+
+            test = true;
+
+            for (int j = 1; j < size; j++) {
+                if (array[j] % i != firstElementMod) {
+                    test = false;
+                    break;
+                }
+            }
+            if (test)
+                System.out.print(i + " ");
+        }
+    }
+
 
 
 
